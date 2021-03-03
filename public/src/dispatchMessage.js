678648015a2e351a69ced4ca8800632e6775f7c8
reminderclock <reminderclock@naver.com>
2021-03-03 21:36:04 +0900

    chore: 모듈 적용

---
+++ public/src/dispatchMessage.js
@@ -0,0 +1,12 @@
+import {inputDispatch} from './main.js';
+inputDispatch.addEventListener('keyup', changeStr);
+
+let asciiArr = [];
+function changeStr() {
+    // console.log(inputDispatch.value);
+    let decAscii = inputDispatch.value.charCodeAt(inputDispatch.value.length-1);
+    let hexAscii = Number(decAscii).toString(16)
+    asciiArr.push(hexAscii);
+    // console.log(asciiArr);
+    inputDispatch.value = asciiArr.join(' ');
+}
--- public/src/main.js
+++ public/src/main.js
@@ -1,13 +1,3 @@
+const inputDispatch = document.getElementById('dispatch');
 
-    const inputDispatch = document.getElementById('dispatch');
-    inputDispatch.addEventListener('keyup', changeStr);
-
-    let asciiArr = [];
-    function changeStr() {
-        // console.log(inputDispatch.value);
-        let decAscii = inputDispatch.value.charCodeAt(inputDispatch.value.length-1);
-        let hexAscii = Number(decAscii).toString(16)
-        asciiArr.push(hexAscii);
-        // console.log(asciiArr);
-        inputDispatch.value = asciiArr.join(' ');
-    }
+export {inputDispatch};
