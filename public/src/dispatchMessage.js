cfe917c3942fb68e96f90201a3fb9b9ac4274f74
reminderclock <reminderclock@naver.com>
2021-03-08 18:58:00 +0900

    fix: 함수 분리

--- public/src/dispatchMessage.js
+++ public/src/dispatchMessage.js
@@ -28,68 +28,38 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 let width = canvas.clientWidth;
 let height = canvas.clientHeight;
+const hexAsciiArr = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 function drawPie() {
-    let value = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-    let degree = 360;
-    const radius = 300;
-    // 총합계
-    let sum = value.reduce((a, b) => a + b);
-    console.log(sum)
-    //conv_array는 비율이 담긴 배열
-    let conv_array = value.slice().map((data)=>{  
-        //값을 총합계로 나누어 비율을 구함
-        let rate = data / sum;  
-        //비율을 360을(degree) 곱해 비율에 따른 도(degree)를 구함.
-        let myDegree = degree * rate;  
-        return myDegree;
-    });
-    console.log(conv_array);
-    //초기화
-    degree = 0;
-    for(let i=0;i < conv_array.length;i++){
-        let item = conv_array[i];
-        // 이전에 그렸던 스타일을 저장
-        ctx.save();
-        // 새로운 경로 만들기
+    const radius = width/2;
+    let degree = 0;
+    let eachHexAngle = (360/hexAsciiArr.length);
+    for(let i=0;i < hexAsciiArr.length;i++){
+        // ctx.save();
         ctx.beginPath();
-        // 시작점 이동
         ctx.moveTo(width/2, height/2);
-        if(i == 0){
-            ctx.arc(width/2, height/2, radius, (Math.PI/180)*0, (Math.PI/180)* item , false);
-            degree = item;
-            console.log(0, degree);
-        } else {
-            ctx.arc(width/2, height/2, radius, (Math.PI/180)*degree, (Math.PI/180)*(degree + item), false);
-            console.log(degree, degree + item);
-            ctx.moveTo(width/2, height/2);
-            degree =  degree + item;
-        }
-        // 현재 하위 경로의 시작부분과 연결된 직선을 추가합니다 .
+        ctx.arc(width/2, height/2, radius, (Math.PI/180)*degree, (Math.PI/180)*(degree + eachHexAngle), false);
+        ctx.moveTo(width/2, height/2);
         ctx.closePath();
-        // 윤곽선을 이용하여 도형을 그립니다.
         ctx.stroke();
-        // 이전 상태 복원하기
-        ctx.restore();
+        degree = degree + eachHexAngle;
+        // ctx.restore();
     }
-
-    // 16진수 문자 그리기
-    let text = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    drawhexAscii();
+}
+function drawhexAscii() {
     let underBar = "_";
-    let numRadsPerLetter = 2*Math.PI / text.length;
+    let numRadsPerLetter = 2*Math.PI / hexAsciiArr.length;
     let radius2 = 250;
-    // 시작각도에서 문자가 써지므로 각 색션 중간보다 조금 앞으로 설정
     let startRotation = (Math.PI/180)*9;
     ctx.font = "bold 30px Serif";
     ctx.fillStyle = 'blue';
     ctx.save();
-    // 중심점으로 이동
     ctx.translate(width/2,height/2);
-    // 라디안 값을 사용하여 이동 (Math.PI/180)*degress
     ctx.rotate(startRotation);
-    for(var i=0;i<text.length;i++){
+    for(var i=0;i<hexAsciiArr.length;i++){
     ctx.save();
     ctx.rotate(i*numRadsPerLetter);
-    ctx.fillText(text[i],0,-radius2);
+    ctx.fillText(hexAsciiArr[i],0,-radius2);
     ctx.fillText(underBar,0,-(radius2-10));
     ctx.restore();
     }
@@ -105,15 +75,10 @@ function init(){
   init();
   
   function arrow(){
-    var now = new Date();
     ctx.save();
     ctx.clearRect(230,230,140,140);
     ctx.translate(width/2,height/2);
     ctx.scale(0.4,0.4);
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = "white";
-    ctx.lineWidth = 8;
-    ctx.lineCap = "round";
 
     sec++;
     if(sec === 360) {
@@ -123,21 +88,25 @@ function init(){
     // 시간 표시 - 초
     ctx.save();
     ctx.rotate(sec * Math.PI/30);
-    ctx.strokeStyle = "#D40000";
-    ctx.fillStyle = "#D40000";
-    ctx.lineWidth = 6;
+    // ctx.strokeStyle = "#D40000";
+    // ctx.fillStyle = "#D40000";
+    ctx.lineWidth = 30;
     ctx.beginPath();
-    ctx.moveTo(-30,0);
-    ctx.lineTo(83,0);
+    ctx.moveTo(-50,0);
+    ctx.lineTo(60,0);
     ctx.stroke();
     ctx.beginPath();
     ctx.arc(0,0,10,0,Math.PI*2,true);
     ctx.fill();
     ctx.beginPath();
-    ctx.arc(95,0,10,0,Math.PI*2,true);
-    ctx.stroke();
+    // ctx.arc(95,0,10,0,Math.PI*2,true);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(60, 30);
+    ctx.lineTo(60, -30);
+    ctx.fill();
+    // ctx.stroke();
     ctx.fillStyle = "rgba(0,0,0,0)";
-    ctx.arc(0,0,3,0,Math.PI*2,true);
+    // ctx.arc(0,0,3,0,Math.PI*2,true);
     ctx.fill();
     ctx.restore();
     ctx.beginPath();
